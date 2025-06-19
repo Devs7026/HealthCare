ebe9e2328f5322fd883ff250bb103f0734d7126a
Devs7026 <d2twins2607@gmail.com>
2025-06-19 23:12:04 +0530

    Made Migrations

--- healthcare_diet_app/backend/backend/settings.py
+++ healthcare_diet_app/backend/backend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'healthapp',
 
@@ -47,6 +48,7 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
