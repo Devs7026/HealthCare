aa253412157fcdbed48533e96f425cb6f42f923d
Devs7026 <d2twins2607@gmail.com>
2025-06-20 22:57:29 +0530

    Made new migrations on date field

--- healthcare_diet_app/backend/healthapp/models.py
+++ healthcare_diet_app/backend/healthapp/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.utils.timezone import now
 
 # Create your models here.
 class FoodLog(models.Model):
     food = models.TextField()
     quantity = models.TextField()
     meal = models.CharField(max_length=50)
-    date = models.DateField()
+    date = models.DateField(default=now)
     created_at = models.DateTimeField(auto_now_add=True)
