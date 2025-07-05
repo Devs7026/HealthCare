28722b959f8361bc4bab5578a5803af06c87979e
Devs7026 <d2twins2607@gmail.com>
2025-07-05 22:16:59 +0530

    Chatbot Folder Structure added

---
+++ AI_Medical_Chatbot/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Dev Srijit',
+    author_email=
+    'd2twins2607@gmail.com',
+    packages= find_packages(),
+    install_requires = [ ]
+)
